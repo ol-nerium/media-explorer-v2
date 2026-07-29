677baf29b77cfffa88f662b5f3749a6fe9f078b5
al-burlachenko <alexandrburl@gmail.com>
2026-07-29 13:04:39 +0300

    create main files structure for the project, create empty functions/actual refs to work with

---
+++ src/js/interfaces/heroInterface.js
@@ -0,0 +1,48 @@
+import { heroSliderData } from "../../main";
+import { heroSectionMarkup } from "../components/hero";
+import { listenersReload, refs } from "../services/refs";
+
+export const heroInterface = (evt) => {
+  const hero = refs.hero.elem();
+
+  const target = evt.target;
+  const elemTagName = evt.target.nodeName;
+
+  const btn = elemTagName === "BUTTON" ? target : null;
+  const btnChild = evt.target.closest("button") ? evt.target : null;
+  const btnData = btn?.dataset ?? btnChild?.closest("button")?.dataset;
+
+  let radioBtnValue = null;
+  if (elemTagName === "INPUT") {
+    radioBtnValue = target.value;
+
+    hero.innerHTML = heroSectionMarkup(heroSliderData, radioBtnValue);
+    listenersReload();
+  }
+
+  const heroInputs = document.querySelectorAll("input");
+  let currentIndex = 0;
+
+  if (btnData?.control === "arrow-left") {
+    currentIndex = Number(Array.from(heroInputs).find((i) => i.checked).value);
+
+    hero.innerHTML = heroSectionMarkup(heroSliderData, currentIndex - 1);
+    listenersReload();
+  }
+  if (btnData?.control === "arrow-right") {
+    currentIndex = Number(Array.from(heroInputs).find((i) => i.checked).value);
+
+    hero.innerHTML = heroSectionMarkup(heroSliderData, currentIndex + 1);
+    listenersReload();
+  }
+  if (btnData?.control === "addToQueque") {
+    alert("LS func should be here + maybe a toast");
+
+    listenersReload();
+  }
+  if (btnData?.control === "showMore") {
+    alert("full card should open");
+
+    listenersReload();
+  }
+};
---
+++ src/js/interfaces/mobileModalMenuInterface.js
@@ -0,0 +1,52 @@
+import { mobileModalMenuMarkup } from "../components/mobileModalMenu";
+import { clickOnNavLink } from "../routingMarkup";
+
+export function onMobHeaderBtnClick(evt) {
+  const target = evt.target.closest("button");
+
+  if (!target) return;
+  const btnControl = target.dataset?.control;
+  console.log(target.dataset?.control);
+  if (btnControl === "openMobileMenu") openModal();
+}
+
+const backdrop = document.querySelector(".backdrop");
+let mobileLinks = null;
+function openModal() {
+  backdrop.classList.remove("is-hidden");
+
+  backdrop.insertAdjacentHTML("afterbegin", mobileModalMenuMarkup());
+
+  const modalCloseBtn = document.querySelector(".modal-closeBtn");
+  modalCloseBtn.addEventListener("click", closeModal);
+  backdrop.addEventListener("click", onBackdropClick);
+  window.addEventListener("keydown", onKeyClose);
+
+  mobileLinks = document.querySelector(".mobile-modal-menu");
+  mobileLinks.addEventListener("click", modaleMenuIntarface);
+}
+
+function closeModal() {
+  mobileLinks.removeEventListener("click", modaleMenuIntarface);
+  backdrop.removeEventListener("click", onBackdropClick);
+  window.removeEventListener("keydown", onKeyClose);
+
+  backdrop.classList.add("is-hidden");
+  backdrop.innerHTML = "";
+}
+
+function onBackdropClick(evt) {
+  console.log(evt.target);
+  if (evt.target === evt.currentTarget) closeModal();
+}
+
+function onKeyClose(evt) {
+  console.log(evt.code);
+  if (evt.code === "Escape") closeModal();
+}
+
+function modaleMenuIntarface(evt) {
+  clickOnNavLink(evt);
+  const isLinkClicked = !!evt.target.closest("a");
+  if (isLinkClicked) closeModal();
+}
